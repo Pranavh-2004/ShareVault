5b563c241172fb5fb733642d2fd230447dfb7a4b
Pranavh-2004 <pranavh2004@gmail.com>
2025-01-11 15:08:52 +0530

    Updated imports

--- src/zkp/proofVerification.ts
+++ src/zkp/proofVerification.ts
@@ -1,8 +1,18 @@
-import { groth16 } from 'snarkjs';
+import { groth16 } from "snarkjs";
+import fs from "fs";
+import path from "path";
 
-export async function verifyProof(proof: any, publicSignals: any): Promise<boolean> {
-  const vkeyPath = path.join(__dirname, 'circom', 'artifacts', 'verification_key.json');
-  const vkey = JSON.parse(fs.readFileSync(vkeyPath, 'utf8'));
+export async function verifyProof(
+  proof: any,
+  publicSignals: any
+): Promise<boolean> {
+  const vkeyPath = path.join(
+    __dirname,
+    "circom",
+    "artifacts",
+    "verification_key.json"
+  );
+  const vkey = JSON.parse(fs.readFileSync(vkeyPath, "utf8"));
 
   return await groth16.verify(vkey, publicSignals, proof);
 }
