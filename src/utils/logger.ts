d20a1a1bc7bbf814d1ed99a3d7209ce9d1f4d410
Pranavjeet-Naidu <pranavjeetnaidu@gmail.com>
2025-01-11 19:50:29 +0530

    added utils

---
+++ src/utils/logger.ts
@@ -0,0 +1,12 @@
+// src/utils/logger.ts
+import * as winston from 'winston';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.Console({ format: winston.format.simple() }),
+  ],
+});
+
+export default logger;
