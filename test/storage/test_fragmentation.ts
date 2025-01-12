79b5791603c8d06de8d9d0d3c419ba480f063572
Pranavh-2004 <pranavh2004@gmail.com>
2025-01-12 14:52:21 +0530

    Added unit tests for storage

---
+++ test/storage/test_fragmentation.ts
@@ -0,0 +1,18 @@
+import { fragmentFile } from "../src/storage/fragmentation";
+
+describe("Fragmentation", () => {
+  test("should split file into correct fragments", () => {
+    const data = Buffer.alloc(5000); // 5KB file
+    const fragments = fragmentFile(data, { chunkSize: 1024 });
+
+    expect(fragments.length).toBe(Math.ceil(5000 / 1024));
+    expect(fragments[0].length).toBe(1024);
+    expect(fragments[fragments.length - 1].length).toBe(5000 % 1024);
+  });
+
+  test("should throw an error for invalid input", () => {
+    expect(() => fragmentFile(Buffer.from(""), { chunkSize: 1024 })).toThrow(
+      "Input buffer is empty"
+    );
+  });
+});
