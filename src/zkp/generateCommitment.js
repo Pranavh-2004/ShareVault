c446e67d13c770b8daed05eb2309449a47b68c68
Pranavjeet-Naidu <pranavjeetnaidu@gmail.com>
2025-01-12 23:47:39 +0530

    i give up

--- src/zkp/generateCommitment.js
+++ src/zkp/generateCommitment.js
@@ -1,11 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.generateCommitment = generateCommitment;
-var js_crypto_1 = require("@iden3/js-crypto");
+var web3_1 = require("web3");
+var web3 = new web3_1.default();
 function generateCommitment(data, randomness) {
     // Convert strings to their numeric representation
     var dataNum = BigInt("0x" + Buffer.from(data).toString("hex"));
     var randomNum = BigInt("0x" + Buffer.from(randomness).toString("hex"));
-    // Use static hash method
-    return js_crypto_1.poseidon.hash([dataNum, randomNum]);
+    // Use web3-based hash method
+    return web3.utils.soliditySha3({ t: "uint256", v: dataNum }, { t: "uint256", v: randomNum });
 }
